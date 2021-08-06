11825c8665ad3e34baef7ee1a4a2f2c963854387
Snehil <snehilwebber@gmail.com>
2021-08-07 03:56:28 +0530

    request shape validation using yup

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,15 +1,66 @@
 import { Router } from "express";
+import * as yup from "yup";
 
 import {
 	handleLogin,
 	handleEditSlug,
 	handleEditDestination,
 } from "../controllers/admin.controllers";
+import validateRequest from "../middlewares/validateRequest";
 
 const router = Router();
 
-router.post("/login", handleLogin);
-router.put("/editSlug/:slug", handleEditSlug);
-router.put("/editDestination/:slug", handleEditDestination);
+const slugSchema = yup
+	.string()
+	.trim()
+	.required("Slug is a required field!")
+	.matches(/^([a-zA-Z0-9]+([-_]*[a-zA-Z0-9]+)*)$/, "Not a valid slug!");
+
+const handleLoginSchema = yup.object().shape({
+	slug: slugSchema,
+	password: yup.string().trim().required("Password is a required field!"),
+});
+
+const slugParamSchema = yup.object().shape({
+	slug: slugSchema,
+});
+
+const handleEditSlugSchema = yup.object().shape({
+	newSlug: yup
+		.string()
+		.trim()
+		.required("Please enter the new slug for the requested update!")
+		.matches(
+			/^([a-zA-Z0-9]+([-_]*[a-zA-Z0-9]+)*)$/,
+			"New slug is not valid! A slug may contain only a-z, A-Z, 0-9, -, and _ symbols."
+		),
+	password: yup.string().trim().required("Password is a required field!"),
+});
+
+const handleEditDestinationSchema = yup.object().shape({
+	newDestination: yup
+		.string()
+		.trim()
+		.required("Please enter the new destination for the requested update!")
+		.matches(
+			/^[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/i,
+			"New destination must be a valid URL!"
+		),
+	password: yup.string().trim().required("Password is a required field!"),
+});
+
+router.post("/login", validateRequest("body", handleLoginSchema), handleLogin);
+router.put(
+	"/editSlug/:slug",
+	validateRequest("params", slugParamSchema),
+	validateRequest("body", handleEditSlugSchema),
+	handleEditSlug
+);
+router.put(
+	"/editDestination/:slug",
+	validateRequest("params", slugParamSchema),
+	validateRequest("body", handleEditDestinationSchema),
+	handleEditDestination
+);
 
 export default router;
--- src/routes/home.routes.ts
+++ src/routes/home.routes.ts
@@ -1,13 +1,46 @@
 import { Router } from "express";
+import { ObjectId } from "mongodb";
+import * as yup from "yup";
 
 import {
-	handleShrinkurl,
+	handleShrinkUrl,
 	handleEnableAnalytics,
 } from "../controllers/home.controllers";
+import validateRequest from "../middlewares/validateRequest";
 
 const router = Router();
 
-router.post("/shrinkurl", handleShrinkurl);
-router.put("/enableanalytics", handleEnableAnalytics);
+const handleShrinkUrlSchema = yup.object().shape({
+	url: yup
+		.string()
+		.trim()
+		.required("URL is a required field!")
+		.matches(
+			/^[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/i,
+			"Not a valid URL!"
+		),
+});
+
+const handleEnableAnalyticsSchema = yup.object().shape({
+	linkId: yup
+		.string()
+		.trim()
+		.required("linkId is required to enable administration!")
+		.test("is-objectId", "Not a valid linkId!", (value, context) =>
+			ObjectId.isValid(value!)
+		),
+});
+
+router.post(
+	"/shrinkurl",
+	validateRequest("body", handleShrinkUrlSchema),
+	handleShrinkUrl
+);
+
+router.put(
+	"/enableanalytics",
+	validateRequest("body", handleEnableAnalyticsSchema),
+	handleEnableAnalytics
+);
 
 export default router;
